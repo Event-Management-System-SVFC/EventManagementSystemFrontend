93ef2eacf042c5b1be9b312111b6036563ffe6db
MariaDBcmd <nolealjacqueline@gmail.com>
2024-12-01 19:43:36 +0800

    home added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [{ path: 'about', component: AboutComponent }];
+
+export const routes: Routes = [{ path: 'about', component: AboutComponent },
+    { path: 'home', component: HomeComponent },
+];
