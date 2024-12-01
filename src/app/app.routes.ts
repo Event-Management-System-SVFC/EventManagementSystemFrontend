7e55bdf3cd3dae4c07c28d38e8a4ac4174a84b03
MariaDBcmd <nolealjacqueline@gmail.com>
2024-12-02 01:05:48 +0800

    Login form added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,11 @@ import { HomeComponent } from './components/home/home.component';
 import { ServicesComponent } from './components/services/services.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { EventlistComponent } from './components/eventlist/eventlist.component';
+import { UserloginComponent } from './components/userlogin/userlogin.component';
+
+
 export const routes: Routes = [{ path: 'about', component: AboutComponent },
     { path: 'home', component: HomeComponent }, { path: 'services', component: ServicesComponent },
     { path: 'dashboard', component: DashboardComponent}, { path: 'eventlist', component: EventlistComponent},
+    { path: 'userlogin', component:UserloginComponent },
 ];
