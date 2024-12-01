c5a76dc39f180081500f892fee817ef4be253daa
MariaDBcmd <nolealjacqueline@gmail.com>
2024-12-01 18:43:17 +0800

    changes added

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { AppComponent } from './app.component';
+import { routes } from './app.routes';
+import { AboutComponent } from './components/about/about.component';
+
+@NgModule({
+  declarations: [AppComponent, AboutComponent],
+  imports: [BrowserModule, RouterOutlet, RouterModule.forRoot(routes)],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
