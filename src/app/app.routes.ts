ea14d2e68f00d22bdc9f1e498fcfb427aeacffbb
MariaDBcmd <nolealjacqueline@gmail.com>
2024-12-01 21:06:17 +0800

    event List added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,8 @@ import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
 import { ServicesComponent } from './components/services/services.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-
-
+import { EventlistComponent } from './components/eventlist/eventlist.component';
 export const routes: Routes = [{ path: 'about', component: AboutComponent },
     { path: 'home', component: HomeComponent }, { path: 'services', component: ServicesComponent },
-    { path: 'dashboard', component: DashboardComponent}
+    { path: 'dashboard', component: DashboardComponent}, { path: 'eventlist', component: EventlistComponent},
 ];
