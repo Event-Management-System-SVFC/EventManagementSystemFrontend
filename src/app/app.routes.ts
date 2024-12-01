5b37d4447038f9de0afee7bdab067758aeffe8ea
MariaDBcmd <nolealjacqueline@gmail.com>
2024-12-01 20:19:12 +0800

    dashboard added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,10 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
 import { ServicesComponent } from './components/services/services.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
 
 export const routes: Routes = [{ path: 'about', component: AboutComponent },
-    { path: 'home', component: HomeComponent }, { path: 'services', component: ServicesComponent }
+    { path: 'home', component: HomeComponent }, { path: 'services', component: ServicesComponent },
+    { path: 'dashboard', component: DashboardComponent}
 ];
