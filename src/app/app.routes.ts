5b9e5863bae1f3aa0a2c73521bb6adad570e1e4d
MariaDBcmd <nolealjacqueline@gmail.com>
2024-12-05 00:31:18 +0800

    Invitation page for attendees been added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,15 @@ import { EventlistComponent } from './components/eventlist/eventlist.component';
 import { UserloginComponent } from './components/userlogin/userlogin.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ForgotpassComponent } from './components/forgotpass/forgotpass.component';
+import { InvitationPageComponent } from './components/invitation-page/invitation-page.component';
+
+
 
 export const routes: Routes = [{ path: 'about', component: AboutComponent },
     { path: 'home', component: HomeComponent }, { path: 'services', component: ServicesComponent },
     { path: 'dashboard', component: DashboardComponent}, { path: 'eventlist', component: EventlistComponent},
     { path: 'userlogin', component:UserloginComponent },  { path: 'register', component:RegisterComponent },
-    { path: 'forgotpass', component:ForgotpassComponent },
+    { path: 'forgotpass', component:ForgotpassComponent }, { path:'invitation-page', component:InvitationPageComponent},
+
 
 ];
---
+++ src/app/components/invitation-page/invitation-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invitation-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './invitation-page.component.html',
+  styleUrl: './invitation-page.component.css'
+})
+export class InvitationPageComponent {
+
+}
